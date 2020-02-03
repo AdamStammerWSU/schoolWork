41cfd0415344cc47635a30fef6da1e58cc9aaaa3
Adam Stammer <adam.stammer@go.winona.edu>
2020-02-03 09:05:30 -0600

    project stuff

---
+++ microcontrollers/lab2/pwmSawtooth/pwmSawtooth.ino
@@ -0,0 +1,34 @@
+int outPin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(outPin, OUTPUT);
+  
+}
+
+void loop() {
+  for(int i = 0; i < 1000; i++) {
+    
+    analogueWrite(i, outPin);
+  }
+
+  for(int i = 800; i > 0; i--) {
+    
+    analogueWrite(sin(i), outPin);
+  }
+}
+
+int analogueWrite(short onCycle, int pin) {
+  if(onCycle == 0) {
+    digitalWrite(pin, LOW);
+  } else if(onCycle == 1000) {
+    digitalWrite(pin, HIGH);
+  } else {
+    //a duty cycle that isn't 1 or 0
+
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(onCycle);
+    digitalWrite(pin, LOW);
+    delayMicroseconds(1000 - onCycle);
+  }
+}
