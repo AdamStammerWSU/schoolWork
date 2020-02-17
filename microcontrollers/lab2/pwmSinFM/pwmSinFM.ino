cd058a911d1aa7882c133ac8fd2bc7a1a450ae95
Adam Stammer <adam.stammer@go.winona.edu>
2020-02-17 10:44:12 -0600

    lab2 info

---
+++ microcontrollers/lab2/pwmSinFM/pwmSinFM.ino
@@ -0,0 +1,25 @@
+int outPin = 3;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(outPin, OUTPUT);
+  
+}
+
+void loop() {
+  analogueWriteSin(200, outPin);
+}
+
+int analogueWrite(float duty, long period, int pin) {
+    digitalWrite(pin, HIGH);
+    delayMicroseconds(duty*period);
+    digitalWrite(pin, LOW);
+    delayMicroseconds(period - (duty*period));
+    
+}
+int analogueWriteSin(long cycle, int pin) {
+  for(int i = 0; i < cycle; i++) {
+    float f = i * 2* 3.14;
+    analogueWrite((sin((f/cycle))+1.0)/2.0, cycle, pin);
+  }
+}
