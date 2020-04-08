0264fe605cfaf005ed6d068dcb33f9429a9f0b40
AdamStammerWSU <adam.stammer@go.winona.edu>
2020-04-08 12:44:47 -0500

    new eclipse workspace setup

---
+++ microcontrollers/quiz3/quiz3.ino
@@ -0,0 +1,48 @@
+/*
+ * Adam Stammer
+ * Microprocessors - Winona State
+ * Spring 2020
+ * Quiz 3
+ * 
+ */
+
+
+//setup variables
+int reading;
+int pin = A0;
+int startTime,t1;
+float voltage, temp;
+
+
+void setup() {
+  //open Serial line
+  Serial.begin(9600);
+  //print data format
+  Serial.println("time(ms),ADC,V,Temp(C),");
+  //capture startTime
+  startTime=millis();
+}
+
+void loop() {
+  //read in from the LM34
+  reading = analogRead(pin);
+  //capture the currrent Time in relation to the start time
+  t1=millis() - startTime;
+
+  //convert adc input to voltage
+  voltage = 5.0*(reading/1023.0);
+  //voltage to temp conversion 10mV/C
+  temp = (voltage * (1.0/0.01)) - 68.0;
+
+  //print out the data (time, reading, voltage, temp)
+  Serial.print(t1);
+  Serial.print(", ");
+  Serial.print(reading);
+  Serial.print(", ");
+  Serial.print(voltage);
+  Serial.print(", ");
+  Serial.print(temp);
+
+  //wait half a second
+  delay(500);
+}
